a8a9881fed522957b18c1dbc1cf82b17a556ac1b
MarcKe <herderkewitz@googlemail.com>
2014-11-17 19:25:11 +0100

    Revert "Switch back to the binary camera HAL"
    This reverts commit 5e43572de01fc08b63dab5adec626c1ac7a5ab2b.
    
    Conflicts:
    	BoardConfig.mk

--- i9300.mk
+++ i9300.mk
@@ -38,6 +38,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/tiny_hw.xml:system/etc/sound/m0
 
+# Camera
+PRODUCT_PACKAGES += \
+    camera.smdk4x12
+
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.smdk4x12
